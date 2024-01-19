1429836e9b81d2b9e486a4dee28d8b5e59cb99f2
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-19 15:50:01 +0530

    setup Nav Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React, {useState, useEffect} from "react";
-import { FaBars} from "react-icons/fa";
 import { BsChatSquareDots} from "react-icons/bs";
 import {
     FaFacebook,
     FaTwitter,
     FaGooglePlusG,
     FaInstagram,
+    FaBars,
 } from 'react-icons/fa';
 const Navbar = () => {
     return <div>
@@ -23,6 +23,33 @@ const Navbar = () => {
                 <a href="#contact">Contact</a>
             </li>
         </ul>
+        <div>
+            <FaFacebook className='mx-4'/>
+            <FaTwitter className='mx-4'/>
+            <FaGooglePlusG className='mx-4'/>
+            <FaInstagram  className='mx-4'/>
+        </div>
+        {/*Hamburger Icon */}
+        <div>
+            <FaBars/>
+        </div>
+        {/*Mobile Menu*/}
+        <div>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="#gallery">Gallery</a>
+                </li>
+                <li>
+                    <a href="#deals">Deals</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
     </div>;
 
 };
