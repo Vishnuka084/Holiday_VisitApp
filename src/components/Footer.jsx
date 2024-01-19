bc740982ee689b192c4132d894f0f049093f190c
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-19 23:44:52 +0530

    footer sections

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BsChatSquareDots} from "react-icons/bs";
+
+const Footer =  () => {
+    return(
+        <div>
+            <div>
+                <BsChatSquareDots/>
+                <h1>Holiday_Visit</h1>
+            </div>
+        </div>
+    );
+};
+
+
+export default Footer
