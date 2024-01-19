7a767623463981adecaa0d7dc8c7e2c3b28757b9
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-19 19:39:47 +0530

    Booking section setup

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -2,6 +2,34 @@ import React from "react";
 
 const Booking = () =>{
     return(
+        <div id='deals'>
+            <form>
+                <div>
+                    <label >Destinations</label>
+                    <select name="" id="">
+                        <option>Grand Antigua</option>
+                        <option>Key West</option>
+                        <option>Maldives</option>
+                        <option>Cozumel</option>
+                    </select>
+                </div>
+
+                <div>
+                    <div>
+                        <label>Check-In</label>
+                        <input type="date"/>
+                    </div>
+                    <div>
+                        <label>Check Out</label>
+                        <input type="date"/>
+                    </div>
+                </div>
+                <div>
+                    <label>Search</label>
+                    <button>Rates & Availability</button>
+                </div>
+            </form>
+        </div>
 
     )
 }
