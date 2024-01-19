5f3a90981867a27252b69dd7721216b38fd110c8
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-19 17:13:52 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,10 @@ import {
     FaBars,
 } from 'react-icons/fa';
 const Navbar = () => {
+const [nav, setNav] = useState(false)
+const handleNav = () => {
+    setNav(!nav)
+}
     return (
         <div className='w-full min-h-[50px] flex justify-between items-center absoult z-10 text-white  bg-gray-700/80'>
             <ul className='hidden sm:flex px-4'>
@@ -24,18 +28,19 @@ const Navbar = () => {
                     <a href="#contact">Contact</a>
                 </li>
             </ul>
-            <div>
+            <div className='flex justify-between'>
                 <FaFacebook className='mx-4'/>
                 <FaTwitter className='mx-4'/>
                 <FaGooglePlusG className='mx-4'/>
                 <FaInstagram  className='mx-4'/>
             </div>
             {/*Hamburger Icon */}
-            <div>
-                <FaBars/>
+            <div className='sm:hidden z-10'>
+                <FaBars size={20} className='mr-4 cursor-pointer'/>
             </div>
             {/*Mobile Menu*/}
-            <div>
+            <div className={'overflow-y-hidden md:hidden ease-in duration-300 absolute text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex flex-col\'\n' +
+                '            : \'absolute top-0 h-screen left-[-100%] ease-in duration-500'}>
                 <ul>
                     <li>
                         <a href="/">Home</a>
@@ -52,7 +57,7 @@ const Navbar = () => {
                 </ul>
             </div>
         </div>
-    )
+    );
 };
 
 export default Navbar
