b1cc6e47de8f10302938eebc45b69b51baa80a00
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-19 19:50:43 +0530

    Booking section setup

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -6,7 +6,7 @@ const Booking = () =>{
             <form className='lg:flex lg:justify-between w-full items-center'>
                 <div className='flex flex-col my-2 py-2'>
                     <label >Destinations</label>
-                    <select className='lg:w-[300px] md:w-full border rounder-md p-2'>
+                    <select className='lg:w-[300px] md:w-full border rounded-md p-2'>
                         <option>Grand Antigua</option>
                         <option>Key West</option>
                         <option>Maldives</option>
@@ -14,12 +14,12 @@ const Booking = () =>{
                     </select>
                 </div>
 
-                <div>
-                    <div>
+                <div className='flex w-full'>
+                    <div className='flex flex-col w-full lg:max-w-[250px] my-2 p-2'>
                         <label>Check-In</label>
                         <input type="date"/>
                     </div>
-                    <div>
+                    <div className='flex flex-col w-full lg:max-w-[250px] my-2 p-2'>
                         <label>Check Out</label>
                         <input type="date"/>
                     </div>
