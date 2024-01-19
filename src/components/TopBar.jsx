633991685348d266b62978b193c179983a55d0de
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-19 15:10:09 +0530

    add icons and top bar setup

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,8 +1,25 @@
 import React from "react";
-
+import {AiFillPhone, AiOutlineClockCircle} from 'react-icons/ai'
+import {BsChatSquareDots} from 'react-icons/bs'
 const TopBar =() => {
     return (
-        <div>TopBar</div>
+        <div>
+            <div>
+                <BsChatSquareDots />
+                <h1>Holiday_Visit</h1>
+            </div>
+            <div>
+                <div>
+                    <AiOutlineClockCircle/>
+                    <p>9AM - 5AM</p>
+                </div>
+                <div>
+                    <AiFillPhone/>
+                    <p>+94 764-615-235</p>
+                </div>
+                <button>Get a Free Quote</button>
+            </div>
+        </div>
     )
 
 }
