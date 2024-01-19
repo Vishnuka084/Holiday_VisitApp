206a070ca7d597846db29911d065bfe6badb1d52
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-19 15:20:23 +0530

    add icons and top bar setup

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -3,14 +3,14 @@ import {AiFillPhone, AiOutlineClockCircle} from 'react-icons/ai'
 import {BsChatSquareDots} from 'react-icons/bs'
 const TopBar =() => {
     return (
-        <div>
-            <div>
-                <BsChatSquareDots />
-                <h1>Holiday_Visit</h1>
+        <div className='flex justify-between items-center px-4 py-2'>
+            <div className='flex items-center'>
+                <BsChatSquareDots size={30} className='text-[var(--primary-dark)] mr-2'/>
+                <h1 className='text-xl font-bold text-gray-700'>Holiday_Visit</h1>
             </div>
-            <div>
-                <div>
-                    <AiOutlineClockCircle/>
+            <div className='flex'>
+                <div className='hidden md:flex items-center px-6'>
+                    <AiOutlineClockCircle size={20} className='mr-2 text-[var(--primary-dark)]'/>
                     <p>9AM - 5AM</p>
                 </div>
                 <div>
