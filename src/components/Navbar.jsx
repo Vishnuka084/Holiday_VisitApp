80cdd1f1687f50fc8add840802388138c5b88a9e
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-19 15:40:21 +0530

    updated

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from "react";
+import { FaBars} from "react-icons/fa";
+import { BsChatSquareDots} from "react-icons/bs";
+import {
+    FaFacebook,
+    FaTwitter,
+    FaGooglePlusG,
+    FaInstagram,
+} from 'react-icons/fa';
+const Navbar = () => {
+    return <div>NavBar</div>;
+
+
+};
+
+export default Navbar
