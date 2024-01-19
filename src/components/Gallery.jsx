f2ae9eefeb98965bf5562d933699c2076acebe83
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-19 23:06:51 +0530

    Booking section setup

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Gallery = () => {
+    return(
+        <div id='gallery'>
+            <h2>Gallery</h2>
+            <div>
+                <div>
+                    <img src="" alt=""/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Gallery
