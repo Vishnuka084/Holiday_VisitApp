eecc7e03f3eaa4ed4c2d288ddb986724d4f38b84
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-19 15:42:43 +0530

    setup Nav Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,22 @@ import {
     FaInstagram,
 } from 'react-icons/fa';
 const Navbar = () => {
-    return <div>NavBar</div>;
-
+    return <div>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="#gallery">Gallery</a>
+            </li>
+            <li>
+                <a href="#deals">Deals</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+        </ul>
+    </div>;
 
 };
 
