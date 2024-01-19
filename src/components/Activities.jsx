76c9ce9983abed12c1d62aecc094aed1748fb055
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-19 19:01:05 +0530

    updated

---
+++ src/components/Activities.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Activities = () => {
+    return (
+        <div>
+            <div>
+                <h3>Resorts</h3>
+                <img src="https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt=""/>
+            </div>
+            <div>
+                <h3>Resorts</h3>
+                <img src="https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt=""/>
+            </div>
+            <div>
+                <h3>Resorts</h3>
+                <img src="https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt=""/>
+            </div>
+
+        </div>
+    )
+}
+
+export default Activities
