6132ecc83e744be47efbe715c0db56df9649b016
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-19 19:45:10 +0530

    Booking section setup

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -4,9 +4,9 @@ const Booking = () =>{
     return(
         <div id='deals' className='max-w-[1140px] m-auto w-full p-4'>
             <form className='lg:flex lg:justify-between w-full items-center'>
-                <div>
+                <div className='flex flex-col my-2 py-2'>
                     <label >Destinations</label>
-                    <select name="" id="">
+                    <select className='lg:w-[300px] md:w-full border rounder-md p-2'>
                         <option>Grand Antigua</option>
                         <option>Key West</option>
                         <option>Maldives</option>
