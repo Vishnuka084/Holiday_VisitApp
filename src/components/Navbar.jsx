85e209ac98616498b48b22664913ab43526c0e7b
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-19 16:31:03 +0530

    setup Nav Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,34 +8,9 @@ import {
     FaBars,
 } from 'react-icons/fa';
 const Navbar = () => {
-    return <div>
-        <ul>
-            <li>
-                <a href="/">Home</a>
-            </li>
-            <li>
-                <a href="#gallery">Gallery</a>
-            </li>
-            <li>
-                <a href="#deals">Deals</a>
-            </li>
-            <li>
-                <a href="#contact">Contact</a>
-            </li>
-        </ul>
-        <div>
-            <FaFacebook className='mx-4'/>
-            <FaTwitter className='mx-4'/>
-            <FaGooglePlusG className='mx-4'/>
-            <FaInstagram  className='mx-4'/>
-        </div>
-        {/*Hamburger Icon */}
-        <div>
-            <FaBars/>
-        </div>
-        {/*Mobile Menu*/}
-        <div>
-            <ul>
+    return (
+        <div className='w-full min-h-[50px] flex justify-between items-center absoult z-10 text-white  bg-gray-700/80'>
+            <ul className='hidden sm:flex px-4'>
                 <li>
                     <a href="/">Home</a>
                 </li>
@@ -49,9 +24,35 @@ const Navbar = () => {
                     <a href="#contact">Contact</a>
                 </li>
             </ul>
+            <div>
+                <FaFacebook className='mx-4'/>
+                <FaTwitter className='mx-4'/>
+                <FaGooglePlusG className='mx-4'/>
+                <FaInstagram  className='mx-4'/>
+            </div>
+            {/*Hamburger Icon */}
+            <div>
+                <FaBars/>
+            </div>
+            {/*Mobile Menu*/}
+            <div>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="#gallery">Gallery</a>
+                    </li>
+                    <li>
+                        <a href="#deals">Deals</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>;
-
+    )
 };
 
 export default Navbar
